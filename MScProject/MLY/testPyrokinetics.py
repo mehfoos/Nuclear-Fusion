26dbd22e8f0d246e4c5e74bf4948bef562a961df
Mehfoos <mehfoos@gmail.com>
2023-08-14 00:22:46 +0100

    reinstalled pyro; tried without loading fields.

--- MScProject/MLY/testPyrokinetics.py
+++ MScProject/MLY/testPyrokinetics.py
@@ -7,7 +7,7 @@ __author__ = "Mehfoos"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-from pyrokinetics import Pyro, template_dir
+from pyrokinetics import Pyro, template_dir, normalisation
 
 def main():
     """ Main entry point of the app """ 
@@ -15,8 +15,9 @@ def main():
     # Path configurations
     # inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc1/gxLinCbc1.in"
     inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc2/gxLinCbc2.in"
+    # inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc2/gxLinCbc2.used_inputs.in"
     # inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/test1/input_MG_MY.in"
-    # outputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc1/gxLinCbc1.out.nc"
+    outputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc2/gxLinCbc2.out.nc"
     # gs2_template = template_dir / "outputs/GS2_linear/gs2.in"
         
     # instantiating the Pyro class as pyro object.
@@ -26,14 +27,17 @@ def main():
     print("GK code check pass?: ", pyro.check_gk_code())
 
     # Load GS2 output data
-    pyro.load_gk_output()
+    pyro.load_gk_output(path=outputFilePath, local_norm=None, \
+        load_fields=True, load_fluxes=True, load_moments=False)
 
     # Print all the data
     print(pyro.gk_output.data_vars)
 
     # Extract signals
     ky = pyro.gk_output["ky"]
-    gamma = pyro.gk_output["gamma"]
+    eigenvalues = pyro.gk_output["eigenvalues"]
+    growth_rate = pyro.gk_output["growth_rate"]
+    mode_freq = pyro.gk_output["mode_frequency"]
 
     # Plot signals
     
