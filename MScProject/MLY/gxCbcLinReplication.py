0b442c237df9f3f360ab7f07a036d0e7ad8a8ec2
Mehfoos <mehfoos@gmail.com>
2023-10-16 01:01:44 +0100

    Plotting tweaks; using new runs for lin CBC.

--- MScProject/MLY/gxCbcLinReplication.py
+++ MScProject/MLY/gxCbcLinReplication.py
@@ -11,6 +11,7 @@ __license__ = "MIT"
 from pyrokinetics import Pyro, template_dir, normalisation
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
 
 def main():
     """ Main entry point of the app """ 
@@ -18,7 +19,7 @@ def main():
     ### IMPORT AND EXTRACT DATA
 
     # Path configurations
-    inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc2/gxLinCbc2.in"
+    inputFilePath = "/home/mly509/GithubCloneMly/Nuclear-Fusion/MScProject/MLY/gxLinCbc5/gxLinCbc5.in"
         
     # instantiating the Pyro class as pyro object.
     pyro = Pyro(gk_file=inputFilePath, gk_code="GS2")
@@ -37,12 +38,14 @@ def main():
 
     ### FILTER /PROCESS DATA
     
-    # Get the final growth rate and 
+    # Get the final growth rate and frequencies
     growth_rate_final = growth_rate.isel(kx=0).max('time')
+    mode_freq_final = mode_freq.isel(kx=0).max('time')
     
     # Get the final growth rate adjusted/scaled as in GX (including coordinates)
     growth_rate_finalGX = growth_rate_final*math.sqrt(2)
-    growth_rate_finalGX = growth_rate_finalGX.assign_coords(ky=growth_rate_finalGX.ky/math.sqrt(2))
+    # growth_rate_finalGX = growth_rate_finalGX.assign_coords(ky=growth_rate_finalGX.ky/math.sqrt(2))
+    mode_freq_finalGX = mode_freq_final*math.sqrt(2)
 
     # Check if equilibrium has been reached
     growth_rate_4thQaurterMean = growth_rate.isel(kx=0).where(growth_rate.time>growth_rate.time[-1]/4).mean('time')
@@ -52,27 +55,57 @@ def main():
     else:
         print("Growth rate likely did not get to equilibrium. Worst case deviation of last quarter of data-series to final value is: ","%.2f" %  growth_rate_maxChange4thQaurterMeanToFinalPct.item(), "%")
 
+
+    ### GX paper data
+    GxFig2ATopGs2 = pd.read_csv('MScProject/MLY/GxFig2ATopGs2.csv',skipinitialspace=True)
+    GxFig2ATopGx = pd.read_csv('MScProject/MLY/GxFig2ATopGx.csv',skipinitialspace=True)
+    GxFig2ABottomGs2 = pd.read_csv('MScProject/MLY/GxFig2ABottomGs2.csv',skipinitialspace=True)
+    GxFig2ABottomGx = pd.read_csv('MScProject/MLY/GxFig2ABottomGx.csv',skipinitialspace=True)
+
     ### PLOT DATA
 
     ## Plot all
 
-    plt.figure(1)
+    fig, ax = plt.subplots()
     growth_rate.isel(kx=0).plot.line(x='time') 
 
-    ## Plot the last time series, without scaling
-    plt.figure(2)
-    # Extract growth rate where kx=0 and time is the max of each series and plot against ky.
-    growth_rate_final.plot.line(x='ky')
-    plt.suptitle('Growth rate where kx=0 and time is the max of each series')
+    fig, ax = plt.subplots()
+    mode_freq.isel(kx=0).plot.line(x='time') 
 
-    ## Plot the last time series, with scaling
-    plt.figure(3)
-    (growth_rate_finalGX).plot.line(x='ky',color="orange",marker="s")
-    plt.suptitle('Replication attempt: GX Figure2 a) Top')
-    plt.xlim((0.0,1.7))
+    ## Plot the last time series, without scaling
+    # fig, ax = plt.subplots()
+    # Extract growth rate where kx=0 and time is the max of each series and plot against ky.
+    # growth_rate_final.plot.line(x='ky')
+    # plt.suptitle('Growth rate where kx=0 and time is the max of each series')
+
+    # fig, ax = plt.subplots()
+    # Extract growth rate where kx=0 and time is the max of each series and plot against ky.
+    # mode_freq_final.plot.line(x='ky')
+    # plt.suptitle('Mode Frequencies where kx=0 and time is the max of each series')
+
+    ## Plot GX paper figure 2.a.top
+    fig, axs = plt.subplots(2,1,layout="constrained")
+    (growth_rate_finalGX).plot.line(x='ky',color="black",marker="o",label="Replication", ax=axs[0])
+    GxFig2ATopGs2.plot(x="x",y="y",label="From GX paper", color="orange",marker="s", markerfacecolor='none', linestyle='None', ax=axs[0])
+    GxFig2ATopGx.plot(x="x",y="y",label="From GX paper", color="blue",marker="x", markerfacecolor='none', linestyle='None', ax=axs[0])
+    plt.suptitle('Replication attempt: GX Figure2 a)')
+    axs[0].set_title('Normalised Growth Rates Frequencies')
+    axs[0].set_xlabel('Normalised binormal wavenumber $k_y$')
     plt.ylim((0.0,0.28))
+    axs[0].set_yticks((0.00,0.1,0.2))
+    axs[0].minorticks_on()
+
+    ## Plot GX paper figure 2.a.bottom
+    (mode_freq_finalGX).plot.line(x='ky',color="black",marker="o",label="Replication", ax=axs[1])
+    GxFig2ABottomGs2.plot(x="x",y="y",label="From GX paper", color="orange",marker="s", markerfacecolor='none', linestyle='None', ax=axs[1])
+    GxFig2ABottomGx.plot(x="x",y="y",label="From GX paper", color="blue",marker="x", markerfacecolor='none', linestyle='None', ax=axs[1])
+    axs[1].sharex(axs[0])
+    plt.xlabel('Normalised binormal wavenumber $k_y$')
+    plt.title('Real (Mode) Frequencies')
+    plt.xlim((0.0,1.7))
+    plt.ylim((-1,1))
     plt.xticks((0.00,0.25,0.50,0.75,1.00,1.25,1.50))
-    plt.yticks((0.00,0.1,0.2))
+    plt.yticks((-1,-0.5,0,0.5,1))
     plt.minorticks_on()
 
     plt.show()
